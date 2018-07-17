8a161d4c7d015f52ade0e4a9eba808968daf8638
Aamir Sehbai <asehbai@gmail.com>
2018-07-17 04:10:14 -0400

    Update startapp.sh

--- scripts/startapp.sh
+++ scripts/startapp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup java -jar spring-boot-2-rest-service-basic-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/app/spring-boot-2-rest-service-basic/target/spring-boot-2-rest-service-basic-0.0.1-SNAPSHOT.jar &
